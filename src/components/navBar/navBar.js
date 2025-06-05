474f1f2138f3ca9180262aa433969160f81890b7
Puja-1998 <ghodkepooja98@gmail.com>
2025-06-05 19:01:18 +0530

    update

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,71 +1,75 @@
-
-import React, { useState } from 'react';
-import {FaBars, FaReact} from 'react-icons/fa';
-import {HiX} from 'react-icons/hi';
-import {Link} from 'react-router-dom';
-import './navBar.css';
+import React, { useState, useEffect } from "react";
+import { FaBars, FaReact } from "react-icons/fa";
+import { HiX } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import "./navBar.css";
+import Type from "../../containers/home/Type";
 
 const data = [
-    {
-        label : 'HOME',
-        to :'/'
-    },
-    {
-        label : 'ABOUT ME',
-        to :'/about'
-    },
-    {
-        label : 'SKILLS',
-        to :'/skills'
-    },
-    {
-        label : 'RESUME',
-        to :'/resume'
-    },
-    {
-        label : 'PROJECTS',
-        to :'/projects'
-    },
-    {
-        label : 'CONTACT ME',
-        to :'/contact'
-    }
-]
+  { label: "HOME", to: "/" },
+  { label: "ABOUT ME", to: "/about" },
+  { label: "SKILLS", to: "/skills" },
+  { label: "RESUME", to: "/resume" },
+  { label: "PROJECTS", to: "/projects" },
+  { label: "CONTACT ME", to: "/contact" },
+];
+
 function Navbar() {
-    const [toggleIcon, setToggleIcon] = useState(false);
+  const [toggleIcon, setToggleIcon] = useState(false);
 
-    const handleToggleIcon = ()=>{
-        setToggleIcon(!toggleIcon);
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+
+  // Prevent scroll when menu is open
+  useEffect(() => {
+    if (toggleIcon) {
+      document.body.classList.add("lock-scroll");
+    } else {
+      document.body.classList.remove("lock-scroll");
     }
-  return (
-    <div>
-        <nav className='navbar'>
-            
-            <div className="navbar__container">
-                <Link to={'/'} className="navbar__container__logo">
-                <FaReact size={30}/>
-                </Link>
-            </div>
+  }, [toggleIcon]);
 
-            <ul className= {`navbar__container__menu ${toggleIcon ? "active" : ""}`} >
-                {
-                    data.map((item, key)=>(
-                        <li key={key} className='navbar__container__menu__item'>
-                            <Link className='navbar__container__menu__item__links' to={item.to}>
-                            {item.label}
-                            </Link>
-                        </li>
-                    ))
-                }
-            </ul>
-            <div className='nav-icons' onClick={handleToggleIcon}>
-                {
-                    toggleIcon ? <HiX size={30}/> : <FaBars size={30}/>
-                }
-            </div>
-        </nav>
-    </div>
-  )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link to="/" className="navbar__container__logo">
+            <h2 className="navbar_logo">
+              Puja Rokade |{" "}
+              <span className="type-writer">
+                <Type />
+              </span>
+            </h2>
+          </Link>
+        </div>
+
+        <ul className={`navbar__container__menu ${toggleIcon ? "active" : ""}`}>
+          {data.map((item, key) => (
+            <li key={key} className="navbar__container__menu__item">
+              <Link
+                className="navbar__container__menu__item__links"
+                to={item.to}
+                onClick={() => setToggleIcon(false)} // Closes menu on click
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className="nav-icons" onClick={handleToggleIcon}>
+          {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+        </div>
+      </nav>
+
+      {/* Overlay for dim background */}
+      <div
+        className={`navbar-overlay ${toggleIcon ? "active" : ""}`}
+        onClick={handleToggleIcon}
+      ></div>
+    </>
+  );
 }
 
 export default Navbar;
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,44 +1,78 @@
-
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import {Animate} from 'react-simple-animate';
-import './home.css';
-import Type from './Type';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import PujaProfile from '../../assets/puja.jpg';
+import Resume from '../../assets/Puja-Resume.pdf';
 
 function Home() {
-
   const navigate = useNavigate();
-  const handleNavigateToContactMePage = ()=>{
-    navigate('/contact')
+  const handleNavigateToContactMePage = () => {
+    navigate("/contact");
+  };
+
+  const handleNavigateToProjectsPage = () =>{
+    navigate("/projects");
   }
 
   return (
-    <section id='home' className='home'>
-      <div className="home__text-wrapper">
-        <h1>Hi, I'm Puja Rokade 
-          <br />
-         <Type/>
-        </h1>
-      </div>
+    <section id="home" className="home-section">
+      <div className="home">
+        <main className="home-main">
+          {/* Right Container */}
+          <div className="right-container">
+            <img
+              src={PujaProfile}
+              alt="home pic"
+              className="profile-img"
+            />
+            <h1 className="name-heading">Puja Rokade</h1>
+            <h3 className="role-heading">Frontend Developer</h3>
+            <div className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/pujaghodke/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin id="social-icon" className="social-icon-linkedin" size={25} />
+              </a>
+              <a
+                href="https://github.com/puja-1998"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub  id="social-icon" className="social-icon-github" size={25}/>
+              </a>
+            </div>
+          </div>
 
-      <Animate 
-      play 
-      duration={1.5} 
-      delay={1} 
-      start={{
-        transform : 'translateY(550px)'
-      }}
-      end={{
-        transform : 'translateX(0px)'
-      }}
-      >
-      <div className="home__contact-me">
-        <button onClick={handleNavigateToContactMePage}>Contact Me</button>
+          {/* Left Container */}
+          <div className="info-container">
+            <h1 className="greeting-text">Hello,</h1>
+            <div className="button-group">
+              <a href={Resume} download="Puja-Resume.pdf" className="button-link">
+                <div className="button-style">Resume</div>
+              </a>
+              <div className="button-style" onClick={handleNavigateToProjectsPage}>Projects</div>
+            </div>
+            <p className="description">
+              I'm Puja, a passionate and creative frontend developer with a love
+              for crafting beautiful and user-friendly digital experiences. My
+              journey in the world of web development began with a curiosity
+              that turned into a genuine fascination for transforming ideas into
+              interactive, visually appealing websites. I'm well-versed in the
+              latest frontend technologies, including HTML5, CSS3, and
+              JavaScript. I have experience working with popular frameworks like
+              React.
+            </p>
+            <div className="button-style contact-btn" onClick={handleNavigateToContactMePage}>
+              Contact Me
+            </div>
+          </div>
+        </main>
       </div>
-      </Animate>
-      
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
